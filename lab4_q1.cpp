e1d270cb94353fdb5723b77c562d3b4671c19158
Soumik77777 <42136372+Soumik77777@users.noreply.github.com>
2018-08-22 10:38:56 +0530

    Add files via upload

---
+++ lab4_q1.cpp
@@ -0,0 +1,32 @@
+//first include the library
+#include<iostream>
+using namespace std;
+
+//conversion between length units
+int main ()
+
+//process
+	{
+
+//declare the main function
+	double centimetrel,metrel,kilometrel;
+
+/*centimetrel is the length in centimetre to be inputted by user
+metrel is the converted length to metre
+kilometrel is the converted length to kilometre*/
+
+	cout<<"Please input length in centimetres:"<<endl;
+
+//conversion formulae
+	cin>>centimetrel;
+	
+	metrel=centimetrel/100;
+	kilometrel=centimetrel/100000;
+
+//result
+	cout<<"The input length is "<<centimetrel<<" centimetres"<<endl;
+	cout<<"The input length is "<<metrel<<" metres"<<endl;
+	cout<<"The input length is "<<kilometrel<<" kilometres"<<endl;
+
+return 0;
+	}
